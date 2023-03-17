d27fb8756eaff017d7aa343105ed81f69764df2d
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-03-17 15:03:14 +0530

    FoodService_ConsumerAndPublisher Completed

---
+++ FoodServicePublisher/src/foodservicepublisher/FoodServiceInterface.java
@@ -0,0 +1,12 @@
+package foodservicepublisher;
+
+public interface FoodServiceInterface {
+
+	public void insertFood();
+
+	public double getFoodPriceById(int foodId);
+
+	public String getFoodNameById(int foodId);
+
+	void getallfoodorder();
+}
