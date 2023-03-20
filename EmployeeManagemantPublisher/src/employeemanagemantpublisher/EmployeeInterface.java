6670b8da1dcdb569d0f95111f6b54fe66b70e517
Shehan <76708200+ShehanEraliyanage@users.noreply.github.com>
2023-03-20 09:06:27 +0530

    remove

--- EmployeeManagemantPublisher/src/employeemanagemantpublisher/EmployeeInterface.java
+++
@@ -1,9 +0,0 @@
-package employeemanagemantpublisher;
-
-public interface EmployeeInterface {
-	public void insertEmployee();
-	public void searchEmployee();
-	public void getallemployee();
-	public void deleteemployee();
-	
-}
--- ResturantDB/src/resturantdb/Database.java
+++
@@ -1,9 +0,0 @@
-package resturantdb;
-
-import java.sql.Connection;
-
-public interface Database {
-
-	public Connection getDatabaseConnection();
-	
-}
